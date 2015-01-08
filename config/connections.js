2b7592c0410eb3b6045baea1802395503dc78619
Vishal Ranjan <Vishivish18@gmail.com>
2015-01-08 23:58:02 +0530

    connection.js

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,11 @@
 module.exports.connections = {
     mongodb: {
         adapter: 'sails-mongo',
-        host: process.env.OPENSHIFT_MONGODB_DB_HOST,
-        port: process.env.OPENSHIFT_MONGODB_DB_PORT,
-        user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-        password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
-        database: 'devesh1234'
+//        host: process.env.OPENSHIFT_MONGODB_DB_HOST,
+//        port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+//        user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+//        password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+//        database: 'devesh1234'
+        url: process.env.OPENSHIFT_MONGODB_DB_URL + "kitty"
     }
 }
